0bd2b299d529a770c0ea6b38ed11b24197ead055
LiuQ-Q <545784977@qq.com>
2020-04-11 19:15:37 +0800

    change api ip address

--- components/Advert.js
+++ components/Advert.js
@@ -3,9 +3,9 @@ import '../public/style/components/advert.css';
 const Advert = () => {
   return (
     <div className="ad-div comm-box">
+      {/* <div><img src="http://blogimages.jspang.com/WechatIMG12.jpeg" width="100%" /></div>
       <div><img src="http://blogimages.jspang.com/WechatIMG12.jpeg" width="100%" /></div>
-      <div><img src="http://blogimages.jspang.com/WechatIMG12.jpeg" width="100%" /></div>
-      <div><img src="http://blogimages.jspang.com/WechatIMG12.jpeg" width="100%" /></div>
+      <div><img src="http://blogimages.jspang.com/WechatIMG12.jpeg" width="100%" /></div> */}
     </div>
   )
 }
--- config/apiUrl.js
+++ config/apiUrl.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const ipUrl = 'http://127.0.0.1:7001/default/';
+const ipUrl = 'http://154.8.231.65:7001/default/';
 const serverPath = {
   getTypeInfo: async () => {
     return await axios.get( ipUrl + 'getTypeInfo/').then(
